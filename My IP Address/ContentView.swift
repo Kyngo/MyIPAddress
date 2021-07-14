b5c094f7f0d5e456a438d8b6548178e8bb478717
Arnau Martin <kyngo@kyngo.net>
2021-07-14 16:28:09 +0200

    Initial Commit

---
+++ My IP Address/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My IP Address
+//
+//  Created by Arnau Martín on 14/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My IP Address/My_IP_AddressApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_IP_AddressApp.swift
+//  My IP Address
+//
+//  Created by Arnau Martín on 14/7/21.
+//
+
+import SwiftUI
+
+@main
+struct My_IP_AddressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
